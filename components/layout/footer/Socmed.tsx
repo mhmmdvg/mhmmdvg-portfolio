b97180a29910b01f243697b212a150c1dba979a4
Muhammad Vikri <57744555+mhmmdvg@users.noreply.github.com>
2022-05-20 01:47:19 +0700

    Update Socmed.tsx

--- components/layout/footer/Socmed.tsx
+++ components/layout/footer/Socmed.tsx
@@ -9,7 +9,7 @@ const Socmed: NextPage = () => {
     <footer className="dark:bg-black bg-white transition pt-10 pb-10 sm:pb-10 flex">
       <div className="container mx-auto">
         <div className="mt-0 w-56 md:w-fit mx-auto dark:text-white text-black sm:mt-10">
-          <div className="flex flex-row space-x-5 dark:bg-[#141414] bg-gray-200 py-2 px-3 md:py-3 md:px-14 rounded-2xl ">
+          <div className="flex flex-row space-x-5 dark:bg-[#141414] bg-gray-200 py-2 px-3 md:py-3.5 md:px-16 rounded-2xl ">
             {socialMedia.map((item) => (
               <CustomTooltip
                 key={item.name}
