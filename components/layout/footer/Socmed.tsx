2b06a656c081d367bd795bb813408cde7dbfcace
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-05-20 05:26:07 +0700

    Resolve responsive footer mobile

--- components/layout/footer/Socmed.tsx
+++ components/layout/footer/Socmed.tsx
@@ -7,7 +7,7 @@ import TooltipCustom from '../../tooltip/Tooltip-custom';
 const Socmed: NextPage = () => {
   return (
     <footer className="dark:bg-black bg-white transition mt-24 mb-10">
-      <div className="min-w-[180px] sm:min-w-[280px] mx-auto h-[72px] w-fit flex dark:bg-[#141414] dark:text-white bg-gray-200 space-x-6 sm:space-x-8 text-black items-center rounded-2xl px-6 sm:px-10 transition">
+      <div className="min-w-[180px] sm:min-w-[280px] mx-auto py-3 w-fit flex dark:bg-[#141414] dark:text-white bg-gray-200 space-x-6 sm:space-x-8 text-black items-center rounded-2xl px-6 sm:px-10 transition">
         {socialMedia.map((item) => (
           <CustomTooltip
             key={item.name}
