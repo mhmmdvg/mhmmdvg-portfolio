766e0ee4e8a4052bf6366c546995c61e268454db
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-05-26 11:50:38 +0700

    Change hover Tools

--- components/pages/home/tools/TechToolsItem.tsx
+++ components/pages/home/tools/TechToolsItem.tsx
@@ -10,7 +10,7 @@ const TechToolsItem: NextPage<ToolsItemProps> = (props) => {
   const { icons, name } = props;
   return (
     // border light DFDFDF
-    <div className=" h-[100px] border cursor-pointer hover:scale-105 duration-150 transition border-[#DFDFDF] dark:border-[#404040] rounded-lg p-2">
+    <div className=" h-[100px] border cursor-pointer ease-in-out hover:scale-110 duration-200 dark:border-[#383838] border-[#eaeaea] rounded-lg p-2">
       <div className="flex flex-row items-center h-full px-4 space-x-2 ">
         <div className="w-full lg:w-1/2 flex justify-center">
           {/* <Image
@@ -24,7 +24,7 @@ const TechToolsItem: NextPage<ToolsItemProps> = (props) => {
         /> */}
           {icons}
         </div>
-        <p className="sm:w-1/2 hidden lg:flex">{name}</p>
+        <p className="font-text sm:w-1/2 hidden lg:flex">{name}</p>
       </div>
     </div>
   );
