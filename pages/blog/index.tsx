4d8201927816d484a7d6f3ef0162db4aa604f666
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-08-10 23:23:00 +0700

    Search Feature

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
 import BlogList from '../../components/pages/blog/BlogList';
 import { getAllPost } from '../../utils/mdx';
 import { PostMeta } from '../../type/mdxType';
 import useLoaded from '../../hooks/use-loading';
 import Header from '../../components/header/Header';
+import Search from '../../components/search/Search';
 
 export async function getStaticProps() {
   const posts = getAllPost('blog').map((post) => post.meta);
@@ -18,6 +19,15 @@ export async function getStaticProps() {
 
 const Blog = ({ posts }: { posts: PostMeta[] }) => {
   const isLoaded = useLoaded();
+  const [data, setData] = useState(posts);
+
+  const searchHandle = (value: string) => {
+    setData(
+      posts.filter((post) => {
+        return post.title.toLowerCase().includes(value.toLowerCase());
+      })
+    );
+  };
 
   return (
     <>
@@ -33,7 +43,12 @@ const Blog = ({ posts }: { posts: PostMeta[] }) => {
         <div className="container mx-auto">
           <div className="flex flex-wrap flex-col items-start mt-10 px-3 lg:px-32 2xl:px-60 sm:items-start sm:justify-start sm:w-full">
             <Header subtitle="I love post anything in here">Blog</Header>
-            <BlogList posts={posts} />
+            <Search
+              fade-in="3"
+              placeholder="Search Post..."
+              onChange={(e) => searchHandle(e.target.value)}
+            />
+            <BlogList posts={data} />
           </div>
         </div>
       </section>
